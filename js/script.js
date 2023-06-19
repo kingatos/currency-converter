6a7f89e76ac8b5a5a071df99ad4c728163ab8fa4
Kinga Tos <toskinga@interia.pl>
2023-06-19 16:48:44 +0200

    formatted code, unused empty spaces

--- js/script.js
+++ js/script.js
@@ -1,86 +1,99 @@
 {
-    const clearButtonResult = () => {
-        const clearButton = document.querySelector(".js-clearButton");
+  const clearButtonResult = () => {
+    const clearButton = document.querySelector(".js-clearButton");
 
-        clearButton.addEventListener("click", () => {
-            document.getElementById("r").innerHTML="";
-        });
-    };
-    
-    const convertAmountFromCurrency = (currencyFromElement) => {
-        const currencyFrom = currencyFromElement.value;
+    clearButton.addEventListener("click", () => {
+      document.getElementById("r").innerHTML = "";
+    });
+  };
 
-        switch (currencyFrom) {
-            case "PLN":
-                return 1.00;
-            case "EUR":
-                return 4.72;
-            case "AUD":
-                return 4.75;
-            case "GBP":
-                return 5.48;
-            case "CHF":
-                return 3.04;
-            case "USD":
-                return 4.72;
-        }
-    };
+  const convertAmountFromCurrency = (currencyFromElement) => {
+    const currencyFrom = currencyFromElement.value;
 
-    const convertAmountToCurrency = (currencyToElement) => {
-        const currencyTo = currencyToElement.value;
+    switch (currencyFrom) {
+      case "PLN":
+        return 1.0;
+      case "EUR":
+        return 4.72;
+      case "AUD":
+        return 4.75;
+      case "GBP":
+        return 5.48;
+      case "CHF":
+        return 3.04;
+      case "USD":
+        return 4.72;
+    }
+  };
 
-        switch (currencyTo) {
-            case "PLN":
-                return 1.00;
-            case "EUR":
-                return 4.72;
-            case "AUD":
-                return 4.75;
-            case "GBP":
-                return 5.48;
-            case "CHF":
-                return 3.04;
-            case "USD":
-                return 4.72;
-        }
-    };
-    
-    const calculateResult = (currencyFromElement, currencyToElement, amount) => {
-        return (convertAmountFromCurrency(currencyFromElement) * amount) / convertAmountToCurrency(currencyToElement);
-    };
-    
+  const convertAmountToCurrency = (currencyToElement) => {
+    const currencyTo = currencyToElement.value;
 
-    const updateResultText = (resultElement, currencyFromElement, currencyToElement, amount, resultCurrency, ) => {
-        const result = calculateResult(currencyFromElement, currencyToElement, amount).toFixed(2);
-        resultElement.classList.add("result-background");
-        resultElement.innerText = `${result} ${resultCurrency}`;
-    };
-       
-    const onFormSubmit = () => {
-        
-        const amountElement = document.querySelector(".js-amount");
-        const currencyFromElement = document.querySelector(".js-currencyFrom");
-        const currencyToElement = document.querySelector(".js-currencyTo");
-        const resultElement = document.querySelector(".js-result");
-        const amount = amountElement.value;
-        const resultCurrency = currencyToElement.value;
+    switch (currencyTo) {
+      case "PLN":
+        return 1.0;
+      case "EUR":
+        return 4.72;
+      case "AUD":
+        return 4.75;
+      case "GBP":
+        return 5.48;
+      case "CHF":
+        return 3.04;
+      case "USD":
+        return 4.72;
+    }
+  };
 
-        updateResultText(resultElement, currencyFromElement, currencyToElement, amount, resultCurrency);
-        
-    };
+  const calculateResult = (currencyFromElement, currencyToElement, amount) => {
+    return (
+      (convertAmountFromCurrency(currencyFromElement) * amount) /
+      convertAmountToCurrency(currencyToElement)
+    );
+  };
 
-    const init = () => {
-        const formElement = document.querySelector(".js-form");
+  const updateResultText = (
+    resultElement,
+    currencyFromElement,
+    currencyToElement,
+    amount,
+    resultCurrency
+  ) => {
+    const result = calculateResult(
+      currencyFromElement,
+      currencyToElement,
+      amount
+    ).toFixed(2);
+    resultElement.classList.add("result-background");
+    resultElement.innerText = `${result} ${resultCurrency}`;
+  };
 
-        formElement.addEventListener("submit",(event) => {
-            event.preventDefault();
-            onFormSubmit();
-            clearButtonResult();
-        });
-    };
+  const onFormSubmit = () => {
+    const amountElement = document.querySelector(".js-amount");
+    const currencyFromElement = document.querySelector(".js-currencyFrom");
+    const currencyToElement = document.querySelector(".js-currencyTo");
+    const resultElement = document.querySelector(".js-result");
+    const amount = amountElement.value;
+    const resultCurrency = currencyToElement.value;
 
-    init();
-    
+    updateResultText(
+      resultElement,
+      currencyFromElement,
+      currencyToElement,
+      amount,
+      resultCurrency
+    );
+  };
+
+  const init = () => {
+    const formElement = document.querySelector(".js-form");
+
+    formElement.addEventListener("submit", (event) => {
+      event.preventDefault();
+      onFormSubmit();
+      clearButtonResult();
+    });
+  };
+
+  init();
 }
-
-
